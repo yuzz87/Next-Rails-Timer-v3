f6330bfdea2f75228a9d239dd0843729bf13f00e
Yamamoto 87 <cs@gmail.com>
2026-05-08 07:23:17 +0900

    update layout

--- timerapp/src/app/layout.tsx
+++ timerapp/src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from "next"
 import Image from "next/image"
 import SwRegister from "./sw-register"
 
+const basePath =
+  process.env.NODE_ENV === "production" ? "/Next-Rails-Timer-v3" : ""
+
 export const metadata: Metadata = {
   title: "dev",
   description: "dev page",
@@ -16,31 +19,55 @@ export default function Layout({
   return (
     <html lang="ja">
       <body>
-        <SwRegister/>
+        <SwRegister />
         {children}
-        
 
         <footer className="fixed bottom-0 left-0 z-50 w-full bg-white">
           <div className="border-t border-gray-300 p-4">
             <div className="flex items-center justify-center gap-10">
               <button className="flex h-16 w-16 items-center justify-center rounded-full bg-green-200">
-                <Image src="/view.svg" alt="viewLogo" width={30} height={30} />
+                <Image
+                  src={`${basePath}/view.svg`}
+                  alt="viewLogo"
+                  width={30}
+                  height={30}
+                />
               </button>
 
               <button className="flex h-16 w-16 items-center justify-center rounded-full bg-green-200">
-                <Image src="/database.svg" alt="databaseLogo" width={30} height={30} />
+                <Image
+                  src={`${basePath}/database.svg`}
+                  alt="databaseLogo"
+                  width={30}
+                  height={30}
+                />
               </button>
 
               <button className="flex h-20 w-20 items-center justify-center rounded-full bg-sky-500">
-                <Image src="/coffee.svg" alt="coffeeLogo" width={50} height={50} />
+                <Image
+                  src={`${basePath}/coffee.svg`}
+                  alt="coffeeLogo"
+                  width={50}
+                  height={50}
+                />
               </button>
 
               <button className="flex h-16 w-16 items-center justify-center rounded-full bg-green-200">
-                <Image src="/search-alert.svg" alt="searchAlertLogo" width={30} height={30} />
+                <Image
+                  src={`${basePath}/search-alert.svg`}
+                  alt="searchAlertLogo"
+                  width={30}
+                  height={30}
+                />
               </button>
 
               <button className="flex h-16 w-16 items-center justify-center rounded-full bg-green-200">
-                <Image src="/user-pen.svg" alt="userPenLogo" width={30} height={30} />
+                <Image
+                  src={`${basePath}/user-pen.svg`}
+                  alt="userPenLogo"
+                  width={30}
+                  height={30}
+                />
               </button>
             </div>
           </div>
